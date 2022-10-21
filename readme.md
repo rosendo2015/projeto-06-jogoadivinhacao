44b11153405dcb0b262392d3f579e10c0cef11b8
rosendo2015 <rosendc30@gmail.com>
2022-10-20 22:31:32 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,32 @@
+# Exercício 01 do nível 05 - Jogo de adivinhar qual o número
+
+![preview](.github/preview.png)
+
+> Trilha Explorer 
+
+> Projeto feito para treino do nível 05 - <b>Exercicio 01 - Java Script</b> construído no curso Explorer da Rocketseat :rocket:. 
+
+## :hammer_and_wrench: Tecnologias
+
+- HTML
+- CSS
+- Javascript
+- Git e Github
+
+## :nut_and_bolt: Principais pontos abordados
+
+- Funções em Javascript
+- Estrutura de dados com objetos;
+- Estrutura de repetição;
+- Operadores comparativos;
+
+## :dart:  Desafio Javascript
+
+- [ ]  Crie uma **função** que irá calcular a média das notas de cada aluno;
+- [ ]  Supondo que a média, para esse concurso é **7**, verifique **se** cada aluno obteve sucesso ou não em entrar no concurso e mostre uma mensagem na tela.
+
+## :mailbox_closed: Contatos
+
+> Email - rosendc30@gmail.com
+
+> Linkedin - https://www.linkedin.com/in/francisco-rosendo-a05623241/
---
+++ script.js
@@ -0,0 +1,57 @@
+/* 1º pegar os botões*/
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const randomNumber = Math.round(Math.random() * 10);
+let xAttempts = 1;
+
+//eventos
+btnTry.addEventListener('click', hendleTryClick);
+btnReset.addEventListener('click', hendleResetClick);
+document.addEventListener('keypress', pressEnter);
+
+//função callback
+function hendleTryClick(event) {
+    event.preventDefault();
+
+    const inputNumber = document.querySelector("#inputNumber");
+
+    if (Number(inputNumber.value) == randomNumber) {
+        toggleScreen()
+        screen2.querySelector("h2").innerText = `Parabéns, você acertou em ${xAttempts} tentativas`;
+    }
+    attemptInvalid();
+    inputNumber.value = "";
+    xAttempts++;
+
+}
+
+function hendleResetClick() {
+    toggleScreen()
+    xAttempts = 1
+    document.location.reload();
+}
+
+function toggleScreen() {
+    screen1.classList.toggle("hide");
+    screen2.classList.toggle("hide");
+}
+
+function pressEnter(e) {
+
+    if (e.key == 'Enter' && screen1.classList.contains('hide')) {
+        hendleResetClick()
+    }
+}
+
+function attemptInvalid() {
+    
+    if ((inputNumber.value == "") || (inputNumber.value < 0) || (inputNumber.value != Number(inputNumber.value))) {
+        --xAttempts;
+       
+        
+    }
+    return false
+}
+
